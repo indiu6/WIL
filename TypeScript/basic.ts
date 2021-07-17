d52e1bd3b393ad7723e0bdc00e08a7ad8dc6372e
Sean Yang <hewas6@gmail.com>
2021-07-17 13:19:45 -0400

    TS basic

---
+++ TypeScript/basic.ts
@@ -0,0 +1,53 @@
+//* It seems TS is very similar to C#
+
+interface IUser {
+  name: string;
+  age: number;
+  isAdult?: boolean;
+}
+
+let user1: IUser = {
+  name: 'Neo',
+  age: 123,
+};
+
+class Animal {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+class Cat extends Animal {
+  getName(): string {
+    return `Cat name is ${this.name}.`;
+  }
+}
+
+let cat: Cat;
+cat = new Cat('Lucy');
+console.log(cat.getName()); // Cat name is Lucy.
+
+// Error - TS2532: Object is possibly 'undefined'.
+function toString(str: string | undefined) {
+  return str.toString();
+}
+
+// Type Assertion
+function toString(str: string | undefined) {
+  return (str as string).toString();
+}
+
+// Optional Chaining
+function toString(str: string | undefined) {
+  return str?.toString();
+}
+
+const foo = null ?? 'Hello nullish.';
+console.log(foo); // Hello nullish.
+
+const bar = false ?? true;
+console.log(bar); // false
+
+const baz = 0 ?? 12;
+console.log(baz); // 0
