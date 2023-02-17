0e0dc40c7a9e31b002f8ab327ada6e0c877c99bb
indiu6 <hewas6@gmail.com>
2023-02-17 00:18:27 -0500

    change space 4 to 2

--- coding-test/ValidParentheses-stack.ts
+++ coding-test/ValidParentheses-stack.ts
@@ -14,44 +14,44 @@
 // Output: false
 
 function isValid(s: string): boolean {
-  //! Using ArrayDeque is faster
-  //! than using Stack class
-  let stack: string[] = []
+    //! Using ArrayDeque is faster
+    //! than using Stack class
+    let stack: string[] = []
 
-  // Traversing the session
-  for (let i = 0; i < s.length; i++) {
-    let x = s[i]
+    // Traversing the session
+    for (let i = 0; i < s.length; i++) {
+        let x = s[i]
 
-    if (x == '(' || x == '[' || x == '{') {
-      // Push the element in the stack
-      stack.push(x)
-      continue
+        if (x == '(' || x == '[' || x == '{') {
+            // Push the element in the stack
+            stack.push(x)
+            continue
+        }
+
+        // If current character is not opening
+        // bracket, then it must be closing.
+        // So stack cannot be empty at this point.
+        if (stack.length == 0) return false
+
+        let check: string | undefined
+        switch (x) {
+            case ')':
+                check = stack.pop()
+                if (check == '{' || check == '[') return false
+                break
+
+            case '}':
+                check = stack.pop()
+                if (check == '(' || check == '[') return false
+                break
+
+            case ']':
+                check = stack.pop()
+                if (check == '(' || check == '{') return false
+                break
+        }
     }
 
-    // If current character is not opening
-    // bracket, then it must be closing.
-    // So stack cannot be empty at this point.
-    if (stack.length == 0) return false
-
-    let check: string | undefined
-    switch (x) {
-      case ')':
-        check = stack.pop()
-        if (check == '{' || check == '[') return false
-        break
-
-      case '}':
-        check = stack.pop()
-        if (check == '(' || check == '[') return false
-        break
-
-      case ']':
-        check = stack.pop()
-        if (check == '(' || check == '{') return false
-        break
-    }
-  }
-
-  // Check Empty Stack
-  return stack.length == 0
+    // Check Empty Stack
+    return stack.length == 0
 }
--- coding-test/twoSum-hashMap.ts
+++ coding-test/twoSum-hashMap.ts
@@ -1,30 +1,30 @@
 function twoSum(nums: number[], target: number): number[] {
-  // const result: number[] = []
+    // const result: number[] = []
 
-  // for (let i = 0; i < nums.length; i++) {
-  //   for (let j = 0; j < nums.length; j++) {
-  //     const firstElement = nums[i]
-  //     const secondElement = nums[j]
+    // for (let i = 0; i < nums.length; i++) {
+    //   for (let j = 0; j < nums.length; j++) {
+    //     const firstElement = nums[i]
+    //     const secondElement = nums[j]
 
-  //     if (firstElement + secondElement === target) {
-  //       result[i] = i
-  //       result[j] = j
-  //     }
-  //   }
-  // }
+    //     if (firstElement + secondElement === target) {
+    //       result[i] = i
+    //       result[j] = j
+    //     }
+    //   }
+    // }
 
-  // return result
+    // return result
 
-  //! solution [3,2,4], 6
-  let map = new Map()
+    //! solution [3,2,4], 6
+    let map = new Map()
 
-  for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i]
-    if (map.has(complement)) {
-      return [map.get(complement), i]
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i]
+        if (map.has(complement)) {
+            return [map.get(complement), i]
+        }
+        map.set(nums[i], i)
     }
-    map.set(nums[i], i)
-  }
 
-  return []
+    return []
 }
