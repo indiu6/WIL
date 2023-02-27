8e2b910772b9e95a92cb483ab84925a90364c14f
indiu6 <hewas6@gmail.com>
2023-02-27 11:55:16 -0500

    add Reverse Linked List

---
+++ coding-test/easy/Reverse Linked List.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# Time complexity: O(N)
+# Space complexity: O(1)
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        current = head
+
+        while current:
+            temp = current.next
+            current.next = prev
+            prev = current
+            current = temp
+
+        return prev
