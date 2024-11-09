d2c5217c0f809e1f6cfe05967be06c575f3b28d5
indiu6 <hewas6@gmail.com>
2024-11-09 09:45:51 -0500

    No use Counter built in func

--- coding-test/easy/RansomeNote.py
+++ coding-test/easy/RansomeNote.py
@@ -3,17 +3,38 @@ from collections import Counter
 
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        d1 = Counter(ransomNote)
-        d2 = Counter(magazine)
-
-        for key, val in d1.items():
-            if key not in d2:
-                return False
+        # without Counter import
+        # Time: O(m + n), Space: O(1)
+        counter = {}
+        for c in magazine:
+            if c in counter:
+                counter[c] += 1
             else:
-                if d2[key] < val:
-                    return False
+                counter[c] = 1
+
+        for c in ransomNote:
+            if c not in counter:
+                return False
+            elif counter[c] == 1:
+                del counter[c]
+            else:
+                counter[c] -= 1
 
         return True
+
+        # Using Counter
+
+        # d1 = Counter(ransomNote)
+        # d2 = Counter(magazine)
+
+        # for key, val in d1.items():
+        #     if key not in d2:
+        #         return False
+        #     else:
+        #         if d2[key] < val:
+        #             return False
+
+        # return True
 
 
 # see TS ver.
