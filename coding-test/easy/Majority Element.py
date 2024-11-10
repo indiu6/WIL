e4230bb0809e361478622671832e75d776ea6b78
indiu6 <hewas6@gmail.com>
2024-11-09 23:52:58 -0500

    Redo majorityElement

--- coding-test/easy/Majority Element.py
+++ coding-test/easy/Majority Element.py
@@ -31,3 +31,25 @@ class Solution:
 
 
 # Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+
+# time O(n), space O(n)
+class Solution2:
+    def majorityElement(self, nums: list[int]) -> int:
+        counter = {}
+
+        for num in nums:
+            if num in counter:
+                counter[num] += 1
+            else:
+                counter[num] = 1
+
+        max_count = -1
+        ans = -1
+
+        for key, val in counter.items():
+            if val > max_count:
+                max_count = val
+                ans = key
+
+        return ans
