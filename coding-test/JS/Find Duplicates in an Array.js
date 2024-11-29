588146d3740e9d8d2ddfcc6543f22695c2e09afb
Sean Yang <hewas6@gmail.com>
2024-11-28 21:20:16 -0500

    Add Find Duplicates in an Array

---
+++ coding-test/JS/Find Duplicates in an Array.js
@@ -0,0 +1,30 @@
+const findDuplicates = (nums) => {
+  const numCount = new Map() // 숫자의 빈도를 기록할 해시맵
+  const duplicates = [] // 중복된 요소를 저장할 배열
+
+  for (const num of nums) {
+    if (numCount.has(num)) {
+      if (numCount.get(num) === 1) duplicates.push(num)
+      numCount.set(num, numCount.get(num) + 1)
+    } else {
+      numCount.set(num, 1)
+    }
+  }
+
+  //   for (const [key, count] of numCount) {
+  //     if (count > 1) duplicates.push(key)
+  //   }
+
+  return duplicates
+}
+
+// 입력:
+nums = [4, 3, 2, 7, 8, 2, 3, 1]
+// 출력:
+// [2, 3]
+console.log('dup :>> ', findDuplicates(nums))
+
+// 입력:
+// nums = [1, 2, 3, 4, 5]
+// 출력:
+// [] (중복 없음)
