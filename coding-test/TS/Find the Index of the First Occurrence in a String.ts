94a5655942ed1d9dc22fb502898f22ed2caa4621
Sean Yang <hewas6@gmail.com>
2024-11-30 19:13:29 -0500

    Add Find the Index of the First Occurrence in a String

---
+++ coding-test/TS/Find the Index of the First Occurrence in a String.ts
@@ -0,0 +1,22 @@
+const strStrIndexOf = (haystack: string, needle: string) => {
+    return haystack.indexOf(needle)
+}
+
+console.log(strStrIndexOf("sadbutsad", "sad")); // 0
+console.log(strStrIndexOf("leetcode", "leeto")); // -1
+console.log(strStrIndexOf("hello", "ll")); // 2
+
+
+const strStr = (haystack: string, needle: string) => {
+    const needleLen = needle.length
+    const haystackLen = haystack.length
+
+    for (let i = 0; i < haystackLen - needleLen; i++) {
+        const element = haystack[i];
+        if (haystack.substring(i, needleLen + i) === needle) {
+            return i
+        }
+    }
+
+    return -1
+}
