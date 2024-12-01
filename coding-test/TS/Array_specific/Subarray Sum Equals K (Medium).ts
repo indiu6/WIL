e01a35b3427986a2c85a46365b7ec85073ff9def
Sean Yang <hewas6@gmail.com>
2024-12-01 13:27:53 -0500

    Add Subarray Sum Equals K (Medium)

---
+++ coding-test/TS/Array_specific/Subarray Sum Equals K (Medium).ts
@@ -0,0 +1,7 @@
+const subarraySum = (nums: number[], k: number) => {
+
+}
+
+console.log(subarraySum([1, 1, 1], 2)); // 2
+console.log(subarraySum([1, 2, 3], 3)); // 2
+console.log(subarraySum([0, 0, 0, 0, 0], 0)); // 15
