012bcb9fa07bf2a608d2427d1cde0e225092a6de
Sean Yang <hewas6@gmail.com>
2024-12-06 23:27:18 -0500

    Add Merge Two Sorted Lists

---
+++ coding-test/TS/Merge Two Sorted Lists.ts
@@ -0,0 +1,15 @@
+// class ListNode {
+//     val: number;
+//     next: ListNode | null;
+
+//     constructor(val?: number, next?: ListNode | null) {
+//         this.val = val ?? 0;
+//         this.next = next ?? null;
+//     }
+// }
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    const dummy = new ListNode(-1); // 결과 리스트의 더미 헤드
+
+    return null
+}
